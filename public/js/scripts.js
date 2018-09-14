26d47abf03ec7a619aa2be059a06467455721ec2
ayoa77 <ayodeleamadi@gmail.com>
2018-09-14 21:52:50 +0800

    pushing working icon order changes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -337,7 +337,13 @@ jQuery(function ($) {
 
 //     }());
 
-
+    $(document).ready(function () {
+        $("a").on("click touchend", function (e) {
+            var el = $(this);
+            var link = el.attr("href");
+            window.location = link;
+        });
+    });
 });
 
 
